3edeb09295559d05954e79587d362c9384aeba0e
suudi <suudiabdulfetah@gmail.com>
2024-04-12 23:18:00 +0300

    feat: Add favoriteBlog() finder and its test

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,7 +1,7 @@
 const { test, describe } = require('node:test');
 const assert = require('node:assert');
 
-const { totalLikes } = require('../utils/helper');
+const { totalLikes, favoriteBlog } = require('../utils/helper');
 
 describe('Total likes', () => {
     test('zero blogs', () => {
@@ -41,4 +41,65 @@ describe('Total likes', () => {
 
         assert.strictEqual(totalLikes(blogs), 10);
     });
+})
+
+describe('Favorite blog', () => {
+    test('zero blogs return - N/A', () => {
+        const blogs = [];
+
+        assert.strictEqual(favoriteBlog(blogs), 'N/A');
+    });
+
+    test('Many blogs with one most favorite', () => {
+
+        const favorite = {
+            title: 'most favorite',
+            likes: '3',
+        };
+
+        const blogs = [
+            favorite,
+            {
+                title: 'meduim favorite',
+                likes: '2',
+            },
+            {
+                title: 'least favorite',
+                likes: '1',
+            },
+        ];
+
+        assert.deepStrictEqual(favoriteBlog(blogs), favorite);
+    });
+
+    test('Many blogs with more than one most favorite', () => {
+
+        const favorite1 = {
+            title: 'first most favorite',
+            likes: '3',
+        };
+
+        const favorite2 = {
+            title: 'second most favorite',
+            likes: '3',
+        };
+
+        const blogs = [
+            favorite1,
+            favorite2,
+            {
+                title: 'meduim favorite',
+                likes: '2',
+            },
+            {
+                title: 'least favorite',
+                likes: '1',
+            },
+        ];
+
+        const result = favoriteBlog(blogs);
+
+        assert(result.title === 'first most favorite' || result.title === 'second most favorite', 'Return one of the most favorite blogs');
+    });
+
 })
--- utils/helper.js
+++ utils/helper.js
@@ -3,7 +3,7 @@ const findSum = (sum, blog) => sum + Number(blog.likes);
 const totalLikes = (blogs) => blogs.reduce(findSum, 0);
 
 const favoriteBlog = (blogs) => {
-    let favorite = { ...blogs[0] };
+    let favorite = blogs.length > 0 ? { ...blogs[0] } : 'N/A';
 
     blogs.map((blog) => {
         if (Number(blog.likes) > Number(favorite.likes)) {
