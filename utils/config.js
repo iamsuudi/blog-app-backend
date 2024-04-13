9c1ea210b30563b9b59df42e06504ded2e2deca5
suudi <suudiabdulfetah@gmail.com>
2024-04-13 03:05:34 +0300

    feat: Add node_env and testing_db

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
-const { PORT, MONGODB_URI } = process.env;
+const { PORT } = process.env;
+
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 
 module.exports = {
     MONGODB_URI,
