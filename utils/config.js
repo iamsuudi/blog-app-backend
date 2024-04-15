fc132ec652419c645173351f092f38d0b250965d
suudi <suudiabdulfetah@gmail.com>
2024-04-15 12:01:18 +0300

    Add bcrypt password hasher

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
-const { PORT } = process.env;
+const { PORT, saltRounds } = process.env;
 
 const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI;
 
 module.exports = {
     MONGODB_URI,
     PORT,
+    saltRounds
 };
