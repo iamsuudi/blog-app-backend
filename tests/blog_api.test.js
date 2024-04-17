816c8fd967dc8ef5a1249586b3d3236febb65b00
suudi <suudiabdulfetah@gmail.com>
2024-04-17 20:06:27 +0300

    Update blogs_api tests and refactor

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,7 +3,6 @@ const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
-const Blog = require('../models/blog');
 const helper = require('./test_helper');
 const User = require('../models/user');
 
@@ -11,9 +10,8 @@ const api = supertest(app);
 
 describe('when there is initially some blogs saved', () => {
     beforeEach(async () => {
-        await Blog.deleteMany({});
-
-        await Blog.insertMany(helper.initialBlogs);
+        await helper.resetUsers();
+        await helper.resetBlogs();
     });
 
     test('blogs are returned as json', async () => {
@@ -29,7 +27,7 @@ describe('when there is initially some blogs saved', () => {
     });
 
     describe('viewing a specific blog', () => {
-        test.skip('succeeds with a valid id', async () => {
+        test('succeeds with a valid id', async () => {
             const blogsAtStart = await helper.blogsInDb();
 
             const blogToView = blogsAtStart[0];
@@ -39,16 +37,16 @@ describe('when there is initially some blogs saved', () => {
                 .expect(200)
                 .expect('Content-Type', /application\/json/);
 
-            assert.deepStrictEqual(response.body, blogToView);
+            assert.strictEqual(response.body.title, blogToView.title);
         });
 
-        test.skip('fails with 404 for non-existing valid-format id', async () => {
-            const nonExistingId = await helper.nonExistingId();
+        test('fails with 404 for non-existing valid-format id', async () => {
+            const nonExistingId = await helper.nonExistingBlogId();
 
             await api.get(`/api/blogs/${nonExistingId}`).expect(404);
         });
 
-        test.skip('fails with 400 for invalid-format id', async () => {
+        test('fails with 400 for invalid-format id', async () => {
             const invalidId = 'yuio7898ed';
 
             await api.get(`/api/blogs/${invalidId}`).expect(400);
@@ -56,23 +54,30 @@ describe('when there is initially some blogs saved', () => {
     });
 
     describe('addition of a new blog', async () => {
-        const user = await User.findOne({ username: 'root' });
+        test('succeeds for a logged in user and valid blog data', async () => {
+            // take snapshot of the blogs on start to check with the end
+            const blogsAtStart = await helper.blogsInDb();
 
-        const loginResponse = await api
-            .post('/api/login')
-            .send({ username: 'root', password: 'iamsuperuser' })
-            .expect(200)
-            .expect('Content-Type', /application\/json/);
+            // select root user to extract his id
+            const user = await User.findOne({ username: 'root' });
 
-        const { token } = loginResponse.body;
+            // login the root user
+            const loginResponse = await api
+                .post('/api/login')
+                .send({ username: 'root', password: 'iamsuperuser' })
+                .expect(200)
+                .expect('Content-Type', /application\/json/);
 
-        const newBlog = {
-            title: 'go is faster than python',
-            author: 'Superuser',
-            userId: user._id.toString(),
-        };
+            // get the token
+            const { token } = loginResponse.body;
 
-        test.skip('succeeds for a logged in user and valid blog data', async () => {
+            const newBlog = {
+                title: 'this is a new blog',
+                author: 'Superuser',
+                userId: user._id.toString(),
+            };
+
+            // create the new blog with his token
             await api
                 .post('/api/blogs')
                 .send(newBlog)
@@ -80,69 +85,89 @@ describe('when there is initially some blogs saved', () => {
                 .expect(201)
                 .expect('Content-Type', /application\/json/);
 
-            const response = await api.get('/api/blogs');
+            // take snapshot of the blogs at end of the action
+            const blogsAtEnd = await helper.blogsInDb();
 
-            const contents = response.body.map((e) => e.title);
+            // assert the number of the blogs are incremented
+            assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1);
 
-            assert.strictEqual(
-                response.body.length,
-                helper.initialBlogs.length + 1,
-            );
-
-            assert(contents.includes('go is faster than python'));
+            // assert the new added title belongs to the end snapshot
+            const contents = blogsAtEnd.map((e) => e.title);
+            assert(contents.includes(newBlog.title));
         });
 
-        test.skip('fails if user is not logged in and valid blog data', async () => {
+        test('fails if user is not logged in and valid blog data', async () => {
+            // take snapshot of the blogs on start to check with the end
+            const blogsAtStart = await helper.blogsInDb();
+
+            // no user is logged in at the start of every test
+            const user = await User.findOne({ username: 'root' });
+
             const newblog = {
-                title: 'go is faster than python',
+                title: 'super freaky',
                 author: 'Superuser',
-                userId: helper.nonExistingId(),
+                userId: user._id.toString(),
             };
 
-            await api
-                .post('/api/blogs')
-                .send(newblog)
-                .set('Authorization', `Bearer ${token}`)
-                .expect(401);
+            await api.post('/api/blogs').send(newblog).expect(401);
 
-            const response = await api.get('/api/blogs');
+            // take snapshot of the blogs at end of the action
+            const blogsAtEnd = await helper.blogsInDb();
 
-            const titles = response.body.map((e) => e.title);
+            // assert the number of the blogs stays the same
+            assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
 
-            assert.strictEqual(
-                response.body.length,
-                helper.initialBlogs.length,
-            );
-
-            assert(!titles.includes('go is faster than python'));
+            // assert the new added title doesn't belong to the end snapshot
+            const contents = blogsAtEnd.map((e) => e.title);
+            assert(!contents.includes(newblog.title));
         });
 
-        test.skip('fails with 400 if the blog title is missing', async () => {
+        test('fails with 400 if the blog title is missing', async () => {
+            const blogsAtStart = await helper.blogsInDb();
+
+            const user = await User.findOne({ username: 'root' });
+
+            const loginResponse = await api
+                .post('/api/login')
+                .send({ username: 'root', password: 'iamsuperuser' })
+                .expect(200)
+                .expect('Content-Type', /application\/json/);
+
+            const { token } = loginResponse.body;
+
             const inValidBlog = {
                 author: 'abuki',
                 url: '7890',
                 likes: '',
                 userId: user._id.toString(),
             };
+
             await api
                 .post('/api/blogs')
                 .send(inValidBlog)
                 .set('Authorization', `Bearer ${token}`)
                 .expect(400);
 
-            const response = await api.get('/api/blogs');
+            // take snapshot of the blogs at end of the action
+            const blogsAtEnd = await helper.blogsInDb();
 
-            const contents = response.body.map((e) => e.title);
-
-            assert.strictEqual(
-                response.body.length,
-                helper.initialBlogs.length,
-            );
-
-            assert(!contents.includes('go is faster than python'));
+            // assert the number of the blogs stays the same
+            assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
         });
 
-        test.skip('fails with 400 if the blog author is missing', async () => {
+        test('fails with 400 if the blog author is missing', async () => {
+            const blogsAtStart = await helper.blogsInDb();
+
+            const user = await User.findOne({ username: 'root' });
+
+            const loginResponse = await api
+                .post('/api/login')
+                .send({ username: 'root', password: 'iamsuperuser' })
+                .expect(200)
+                .expect('Content-Type', /application\/json/);
+
+            const { token } = loginResponse.body;
+
             const inValidBlog = {
                 title: 'data science is meh',
                 url: '7890',
@@ -156,51 +181,17 @@ describe('when there is initially some blogs saved', () => {
                 .set('Authorization', `Bearer ${token}`)
                 .expect(400);
 
-            const response = await api.get('/api/blogs');
+            // take snapshot of the blogs at end of the action
+            const blogsAtEnd = await helper.blogsInDb();
 
-            const contents = response.body.map((e) => e.title);
-
-            assert.strictEqual(
-                response.body.length,
-                helper.initialBlogs.length,
-            );
-
-            assert(!contents.includes('data science is meh'));
+            // assert the number of the blogs stays the same
+            assert.strictEqual(blogsAtEnd.length, blogsAtStart.length);
         });
 
-        test.skip('if the likes property is missing, it defaults to 0', async () => {
-            await api
-                .post('/api/blogs')
-                .send(newBlog)
-                .set('Authorization', `Bearer ${token}`)
-                .expect(201);
-
-            const response = await api.get(`/api/blogs`);
-
-            assert.strictEqual(
-                response.body.length,
-                helper.initialBlogs.length + 1,
-            );
-
-            const likes = response.body.map((e) => e.likes);
-
-            assert(likes.includes(0));
-        });
-    });
-
-    describe('deletion of a blog', async () => {
-        const user = await User.findOne({ username: 'root' });
-
-        const newBlog = {
-            title: 'go is faster than python',
-            author: 'Superuser',
-            url: '7890',
-            likes: '',
-            userId: user._id.toString(),
-        };
-
-        test.skip('succeeds with 204 for an authorized user and valid blog-id', async () => {
+        test('if the likes property is missing, it defaults to 0', async () => {
             const blogsAtStart = await helper.blogsInDb();
+
+            const user = await User.findOne({ username: 'root' });
 
             const loginResponse = await api
                 .post('/api/login')
@@ -210,14 +201,40 @@ describe('when there is initially some blogs saved', () => {
 
             const { token } = loginResponse.body;
 
-            const savedResponse = await api
+            const newBlog = {
+                title: 'this is a new blog',
+                author: 'Superuser',
+                userId: user._id.toString(),
+            };
+            await api
                 .post('/api/blogs')
                 .send(newBlog)
                 .set('Authorization', `Bearer ${token}`)
-                .expect(201)
+                .expect(201);
+
+            const blogsAtEnd = await helper.blogsInDb();
+
+            assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1);
+
+            const likes = blogsAtEnd.map((e) => e.likes);
+
+            assert(likes.includes(0));
+        });
+    });
+
+    describe('deletion of a blog', async () => {
+        test('succeeds with 204 for an authorized user and valid blog-id', async () => {
+            const blogsAtStart = await helper.blogsInDb();
+
+            const blogToBeDeleted = blogsAtStart[0];
+
+            const loginResponse = await api
+                .post('/api/login')
+                .send({ username: 'root', password: 'iamsuperuser' })
+                .expect(200)
                 .expect('Content-Type', /application\/json/);
 
-            const blogToBeDeleted = savedResponse.body;
+            const { token } = loginResponse.body;
 
             await api
                 .delete(`/api/blogs/${blogToBeDeleted.id}`)
@@ -228,20 +245,20 @@ describe('when there is initially some blogs saved', () => {
 
             const titles = blogsAtEnd.map((e) => e.title);
 
-            assert.strictEqual(blogsAtStart.length, blogsAtEnd.length);
+            assert.strictEqual(blogsAtStart.length, blogsAtEnd.length + 1);
             assert(!titles.includes(blogToBeDeleted.title));
         });
 
-        test.skip('fails with 401 for an unauthorized user and valid blog-id', async () => {
+        test('fails with 401 for an unauthorized user and valid blog-id', async () => {
             const blogsAtStart = await helper.blogsInDb();
 
-            const loginResponse = await api
+            const blogToBeDeleted = blogsAtStart[0];
+
+            await api
                 .post('/api/login')
                 .send({ username: 'root', password: 'iamsuperuser' })
                 .expect(200)
                 .expect('Content-Type', /application\/json/);
-
-            const { token } = loginResponse.body;
 
             const person1LoginResponse = await api
                 .post('/api/login')
@@ -251,15 +268,6 @@ describe('when there is initially some blogs saved', () => {
 
             const { person1Token } = person1LoginResponse.body;
 
-            const savedResponse = await api
-                .post('/api/blogs')
-                .send(newBlog)
-                .set('Authorization', `Bearer ${token}`)
-                .expect(201)
-                .expect('Content-Type', /application\/json/);
-
-            const blogToBeDeleted = savedResponse.body;
-
             await api
                 .delete(`/api/blogs/${blogToBeDeleted.id}`)
                 .set('Authorization', `Bearer ${person1Token}`);
@@ -268,12 +276,12 @@ describe('when there is initially some blogs saved', () => {
 
             const titles = blogsAtEnd.map((e) => e.title);
 
-            assert.strictEqual(blogsAtStart.length, blogsAtEnd.length - 1);
+            assert.strictEqual(blogsAtStart.length, blogsAtEnd.length);
             assert(titles.includes(blogToBeDeleted.title));
         });
 
-        test.skip('fails with 404 for non-existing blog-id', async () => {
-            const nonExistingId = await helper.nonExistingId();
+        test('fails with 404 for non-existing blog-id', async () => {
+            const nonExistingId = await helper.nonExistingBlogId();
 
             const loginResponse = await api
                 .post('/api/login')
@@ -289,7 +297,7 @@ describe('when there is initially some blogs saved', () => {
                 .expect(404);
         });
 
-        test.skip('fails with 400 for invalid-format blog-id', async () => {
+        test('fails with 400 for invalid-format blog-id', async () => {
             const inValidId = '897tuy';
 
             const loginResponse = await api
@@ -308,24 +316,10 @@ describe('when there is initially some blogs saved', () => {
     });
 
     describe('updating of a blog', async () => {
-        const user = await User.findOne({ username: 'root' });
-
-        const newBlog = {
-            title: 'go is faster than python',
-            author: 'Superuser',
-            url: '7890',
-            likes: '',
-            userId: user._id.toString(),
-        };
-
         const updatedBlog = {
-            title: 'go is way more faster than python',
+            title: 'this is updated blog',
             author: 'Superuser',
-            url: '7890',
-            likes: '',
-            userId: user._id.toString(),
         };
-
         test('succeeds for an authorized user and valid blog-id', async () => {
             const loginResponse = await api
                 .post('/api/login')
@@ -334,14 +328,9 @@ describe('when there is initially some blogs saved', () => {
 
             const { token } = loginResponse.body;
 
-            const savedResponse = await api
-                .post('/api/blogs')
-                .send(newBlog)
-                .set('Authorization', `Bearer ${token}`)
-                .expect(201)
-                .expect('Content-Type', /application\/json/);
+            const blogsAtStart = await helper.blogsInDb();
 
-            const blogToBeUpdated = savedResponse.body;
+            const blogToBeUpdated = blogsAtStart[0];
 
             await api
                 .put(`/api/blogs/${blogToBeUpdated.id}`)
@@ -356,21 +345,9 @@ describe('when there is initially some blogs saved', () => {
         });
 
         test('fails for an unauthorized user and valid blog-id', async () => {
-            const loginResponse = await api
-                .post('/api/login')
-                .send({ username: 'root', password: 'iamsuperuser' })
-                .expect(200);
+            const blogsAtStart = await helper.blogsInDb();
 
-            const { token } = loginResponse.body;
-
-            const savedResponse = await api
-                .post('/api/blogs')
-                .send(newBlog)
-                .set('Authorization', `Bearer ${token}`)
-                .expect(201)
-                .expect('Content-Type', /application\/json/);
-
-            const blogToBeUpdated = savedResponse.body;
+            const blogToBeUpdated = blogsAtStart[0];
 
             const person1LoginResponse = await api
                 .post('/api/login')
@@ -381,7 +358,11 @@ describe('when there is initially some blogs saved', () => {
             await api
                 .put(`/api/blogs/${blogToBeUpdated.id}`)
                 .send(updatedBlog)
-                .set('Authorization', `Bearer ${person1LoginResponse.body.token}`);
+                .set(
+                    'Authorization',
+                    `Bearer ${person1LoginResponse.body.token}`,
+                )
+                .expect(401);
 
             const blogsAtEnd = await helper.blogsInDb();
 
@@ -391,7 +372,7 @@ describe('when there is initially some blogs saved', () => {
         });
 
         test('fails with 404 for non-existing id', async () => {
-            const nonExistingId = await helper.nonExistingId();
+            const nonExistingId = await helper.nonExistingBlogId();
 
             const loginResponse = await api
                 .post('/api/login')
@@ -399,12 +380,6 @@ describe('when there is initially some blogs saved', () => {
                 .expect(200);
 
             const { token } = loginResponse.body;
-
-            await api
-                .post('/api/blogs')
-                .send(newBlog)
-                .set('Authorization', `Bearer ${token}`)
-                .expect(201);
 
             await api
                 .put(`/api/blogs/${nonExistingId}`)
@@ -422,12 +397,6 @@ describe('when there is initially some blogs saved', () => {
                 .expect(200);
 
             const { token } = loginResponse.body;
-
-            await api
-                .post('/api/blogs')
-                .send(newBlog)
-                .set('Authorization', `Bearer ${token}`)
-                .expect(201);
 
             await api
                 .put(`/api/blogs/${inValidId}`)
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,5 +1,7 @@
+const bcrypt = require('bcrypt');
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const config = require('../utils/config');
 
 const initialBlogs = [
     {
@@ -13,6 +15,12 @@ const initialBlogs = [
         author: 'fato',
         url: 'iuoiu8',
         likes: 5,
+    },
+    {
+        title: 'go is faster than python',
+        author: 'Superuser',
+        url: '09oip',
+        likes: 9,
     },
 ];
 
@@ -34,12 +42,27 @@ const initialUsers = [
     },
 ];
 
-const nonExistingId = async () => {
+const nonExistingBlogId = async () => {
     const blog = new Blog({ title: 'will be removed soon', author: 'nobody' });
 
     await blog.save();
     await blog.deleteOne();
     return blog._id.toString();
+};
+
+const nonExistingUserId = async () => {
+    const passwordHash = await bcrypt.hash('999', Number(config.saltRounds));
+
+    const user = new User({
+        username: 'iamnothing',
+        name: 'nobody',
+        password: passwordHash,
+    });
+
+    await user.save();
+    await user.deleteOne();
+
+    return user._id.toString();
 };
 
 const blogsInDb = async () => {
@@ -54,10 +77,70 @@ const usersInDb = async () => {
     return users.map((user) => user.toJSON());
 };
 
+const registerUser = async (userData) => {
+    const { username, name, password } = userData;
+
+    const passwordHash = await bcrypt.hash(password, Number(config.saltRounds));
+
+    const user = new User({ username, name, passwordHash });
+
+    return user.save();
+};
+
+const resetUsers = async () => {
+    await User.deleteMany({});
+
+    await registerUser(initialUsers[0]);
+
+    await registerUser(initialUsers[1]);
+
+    return registerUser(initialUsers[2]);
+};
+
+const createBlog = async (blog) => {
+    const user = await User.findOne({ username: 'root' });
+    const userId = user._id.toString();
+
+    const { title, author, url } = blog;
+    let { likes } = blog;
+
+    if (!likes) likes = 0;
+
+    const newBlog = new Blog({
+        title,
+        author,
+        url,
+        likes,
+        user: userId,
+    });
+
+    const savedBlog = await newBlog.save();
+
+    return User.findByIdAndUpdate(userId, {
+        username: user.username,
+        name: user.name,
+        passwordHash: user.passwordHash,
+        blogs: user.blogs.concat(savedBlog._id),
+    });
+};
+
+const resetBlogs = async () => {
+    await Blog.deleteMany({});
+
+    await createBlog(initialBlogs[0]);
+
+    await createBlog(initialBlogs[1]);
+
+    return createBlog(initialBlogs[2]);
+};
+
 module.exports = {
     initialBlogs,
     initialUsers,
-    nonExistingId,
+    nonExistingUserId,
+    nonExistingBlogId,
     blogsInDb,
     usersInDb,
+    resetBlogs,
+    resetUsers,
 };
