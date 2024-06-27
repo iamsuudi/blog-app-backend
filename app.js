6651b6ddd67246f7775549ea6ee826236f5de6bb
Abdulfetah Suudi <suudiabdulfetah@gmail.com>
2024-06-27 17:13:37 +0300

    feat: serve static blogThumbnails

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(
     }),
 );
 app.use(express.static('dist'));
+app.use(express.static('blogThumbnails'));
 app.use(express.json());
 app.use(
     session({
--- routers/user.router.js
+++ routers/user.router.js
@@ -4,9 +4,10 @@ const userController = require('../controllers/users.controller');
 const userRouter = express.Router();
 
 userRouter.get('/users', userController.getAllUsers);
-userRouter.get('/users/me', userController.getMe);
 userRouter.get('/users/:userId', userController.getUser);
-userRouter.put('/users/update', userController.updateMe);
+
+userRouter.get('/users/me', userController.getMe);
+userRouter.put('/users/me', userController.updateMe);
 userRouter.delete('/users/delete', userController.deleteMe);
 
 module.exports = userRouter;
